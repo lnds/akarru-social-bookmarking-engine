7c8c2461d657e9e47964dd0a9fc755ddfb833d4e
eadiaz <eadiaz@8bafb967-bd0d-0410-aeb5-8d584dead05f>
2006-03-05 00:26:08 +0000

    Limit pages to 50 until we have a better pagination algorithm
    git-svn-id: svn://svn.code.sf.net/p/akarru/code/trunk@7 8bafb967-bd0d-0410-aeb5-8d584dead05f

--- public_html/index.php
+++ public_html/index.php
@@ -4,6 +4,12 @@
   $memes = new memes($bm_db, $bm_user, $bm_promo_level);
 
   $smarty->assign('memes', $memes->get_memes($page, '', $memes->records_to_page*50 ));
+  if ($memes->pages > 50) {
+	  $memes->pages = 50;
+  }
+  if ($bm_users->is_logged_in()) {
+	  $smarty->assign('special_message', '<p><strong>¿Puedes escribir un relato con s&oacute;lo 100 palabras?</strong><br/><a href="100words.php" style="color:white;font-width:bold;font-size:12pt;">escribe un micro cuento</a></p>');
+  }
   if ($memes->pages > 1) 
 	  $smarty->assign('pages', $memes->pages+1);
   $smarty->assign('$bm_message', 'estos memes han sido promovidos recientemente a la primera p&aacute;na');
