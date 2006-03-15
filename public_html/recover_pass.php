dbd8339d2500d22f0276b1f655b651184159e1e9
eadiaz <eadiaz@8bafb967-bd0d-0410-aeb5-8d584dead05f>
2006-03-15 14:28:45 +0000

    Change recover_pass.php, recover_pass_form.php was removed from the project and new templates added. Also site.conf modified with new messages supporting password recovering. Fixes bugs on email sending of passwords.
    git-svn-id: svn://svn.code.sf.net/p/akarru/code/trunk@40 8bafb967-bd0d-0410-aeb5-8d584dead05f

--- public_html/recover_pass.php
+++ public_html/recover_pass.php
@@ -1,17 +1,19 @@
 <?php
   include_once('akarru.lib/common.php');
   if (empty($_POST['email'])) {
-	  $bm_content = 'akarru.gui/recover_pass_form.php';
+	  $bm_content = 'recover_pass';
   }
   else
   {
+	  $email = $_POST['email'];
 	  $subject = '['.$bm_site_name.'] '.$bl_recover;
 	  $pass = $bm_users->gen_password($email, $subject, $bf_recover_pass, $bm_login_url);
 	  $bm_info_text = $bl_password_sent;
 	  $bm_info_title = '&nbsp;' ;
-	  $bm_content = 'akarru.gui/info.php';
+	  $bm_content = 'password_sent';
   }
-  $bm_no_folkbar = true;
-  $bm_title = $bl_recover_pass;
-  include_once('akarru.gui/master_page.php');
+  $smarty->assign('content_title', $bl_recover_pass);
+  $smarty->assign('content', $bm_content);
+  $smarty->assign('show_ads', true);
+  $smarty->display('master_page.tpl');
 ?>
---
+++ smarty/templates/recover_pass.tpl
@@ -0,0 +1,11 @@
+{include file="form_header.tpl"}
+<table border="0" cellpadding="2" cellspacing="0" width="360">
+<tr><td class="view-label-class"><font color="red">*</font> <b>{#label_login_email#}</b></td></tr>
+<tr><td><input type="text" name="email" class="view-input-class" size="30" /></td></tr>
+{if $error_emai}
+<tr><td class="error">{#error_email#}</td></tr>
+{/if}
+<tr><td><input type="submit" name="recover_pass" value="{#label_recover_pass_submit#}" /></td></tr>
+</table>
+{include file="form_footer.tpl"}
+<div style="height:400px">&nbsp;</div>
