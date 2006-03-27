29cc541f4e2312c1d0f6712ed72bdb375913378c
eadiaz <eadiaz@8bafb967-bd0d-0410-aeb5-8d584dead05f>
2006-03-27 16:19:03 +0000

    Added support for persistent session.
    git-svn-id: svn://svn.code.sf.net/p/akarru/code/trunk@67 8bafb967-bd0d-0410-aeb5-8d584dead05f

--- public_html/login.php
+++ public_html/login.php
@@ -18,7 +18,7 @@
 		  $bm_errors++;
 	  }
 	  if ($bm_errors == 0) {
-		  if ($bm_users->do_login($_POST['user'],$_POST['pass'])) {
+		  if ($bm_users->do_login($_POST['user'],$_POST['pass'],$_POST['remember'])) {
 			  $url = $_POST['from'];
 			  header("Location: $url");
 			  exit;
@@ -33,8 +33,16 @@
   }
   else
   {
-	  $from = $_GET['from'].'.php';
+	  $from = $_GET['from'].'.php?post=1';
+	  if (!empty($_GET['url'])) {
+		  $from .= '&url='.$_GET['url'];
+	  }
+	  if (!empty($_GET['title'])) {
+		  $from .= '&title='.$_GET['title'];
+	  }
+
   }
+  $smarty->assign('community', true);
   $smarty->assign('content_title', 'login');
   $smarty->assign('from', $from);
   $smarty->assign('content', 'login');
