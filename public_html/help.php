3bd73de17b7a2b52f5e09e7e6e914148ba581b6a
eadiaz <eadiaz@8bafb967-bd0d-0410-aeb5-8d584dead05f>
2006-03-27 16:17:26 +0000

    help.php was doing unnecesary work
    git-svn-id: svn://svn.code.sf.net/p/akarru/code/trunk@65 8bafb967-bd0d-0410-aeb5-8d584dead05f

--- public_html/help.php
+++ public_html/help.php
@@ -1,13 +1,9 @@
 <?php
   $page = $_GET['page'];
   include_once('akarru.lib/common.php');
-  $smarty->assign('content_title', 'preguntas frecuentes');
-  $memes = new memes($bm_db, $bm_user, $bm_promo_level);
-  $smarty->assign('memes', $memes->get_memes($page));
-  if ($memes->pages > 1) 
-	  $smarty->assign('pages', $memes->pages+1);
-  $smarty->assign('$bm_message', 'estos memes han sido promovidos recientemente a la primera p&aacute;na');
+  $smarty->assign('content_title', $bl_help);
   $smarty->assign('content', 'help');
+  $smarty->assign('community', true);
   $smarty->display('master_page.tpl');
 ?>
 
