ce49ab922b9f9cbd8752deaaf535199851066190
eadiaz <eadiaz@8bafb967-bd0d-0410-aeb5-8d584dead05f>
2006-03-27 16:26:05 +0000

    ??
    git-svn-id: svn://svn.code.sf.net/p/akarru/code/trunk@71 8bafb967-bd0d-0410-aeb5-8d584dead05f

--- public_html/unpopular.php
+++ public_html/unpopular.php
@@ -3,7 +3,7 @@
   $page = $_GET['page'];
   $smarty->assign('content_title', 'lo menos popular');
   $memes = new memes($bm_db, $bm_user, $bm_promo_level);
-  $smarty->assign('memes', $memes->get_memes($page, 'order by vote_count asc'));
+  $smarty->assign('memes', $memes->get_memes($page, 'order by votes asc'));
   if ($memes->pages > 1) 
 	  $smarty->assign('pages', $memes->pages+1);
   $smarty->assign('show_ads', $page < $memes->pages);
