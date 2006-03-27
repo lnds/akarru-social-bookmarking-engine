36ad5518711d88513d84e90feedf61fd495899c2
eadiaz <eadiaz@8bafb967-bd0d-0410-aeb5-8d584dead05f>
2006-03-27 16:13:05 +0000

    added user as parameter for click method.
    git-svn-id: svn://svn.code.sf.net/p/akarru/code/trunk@61 8bafb967-bd0d-0410-aeb5-8d584dead05f

--- public_html/ajax_click.php
+++ public_html/ajax_click.php
@@ -7,7 +7,7 @@ print '<?xml version="1.0" encoding="ISO-8859-1" standalone="yes" ?>';
 <?php
 if(isset($_GET['meme_id'])){
 	$bm_memes = new memes($bm_db, $bm_user, $bm_promo_level);
-	$bm_memes->click($_GET['meme_id']);
+	$bm_memes->click($_GET['meme_id'], $bm_user);
 	echo "<data>".$bm_memes->get_votes($_GET['meme_id'])."</data>";
 	echo "<meme_id>".$_GET['meme_id']."</meme_id>";
 }
