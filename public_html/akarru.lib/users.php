02e01ddce27a11601582e98e724f6cbe6c7a1d6d
eadiaz <eadiaz@8bafb967-bd0d-0410-aeb5-8d584dead05f>
2006-03-27 15:08:03 +0000

    Added persistent sessions, uses a secure algorithm, please check your database structure.
    git-svn-id: svn://svn.code.sf.net/p/akarru/code/trunk@54 8bafb967-bd0d-0410-aeb5-8d584dead05f

--- public_html/akarru.lib/users.php
+++ public_html/akarru.lib/users.php
@@ -7,19 +7,37 @@ class users {
 	function users($db)
 	{
 		$this->db = $db;
-		$this->user = $_SESSION['user_data'];
+		$psession = $_COOKIE["bm_login_cookie"];
+		if (empty($psession)) 
+			$this->user = $_SESSION['user_data'];
+		else
+		{
+			$this->user = $this->db->fetch_object("select * from users where persistent_session = '$psession'");
+			$_SESSION['user_data'] = $this->user;
+		}
 	}
 
 	function is_logged_in(){
-		$this->user = $_SESSION['user_data'];
+		$psession = $_COOKIE["bm_login_cookie"];
+		if (empty($psession)) 
+			$this->user = $_SESSION['user_data'];
+		else
+		{
+			$this->user = $this->db->fetch_object("select * from users where persistent_session = '$psession'");
+			if ($this->user == FALSE) 
+				unset($this->user);
+		}
 		return isset($this->user);
 	}
 
 	function logoff(){
+		$psession = $_COOKIE["bm_login_cookie"];
+		$this->db->execute("update users set persistent_session = null where persistent_session = '$psession'");
 		@session_destroy();
 		@session_unset();
 		$sessionid=session_name();
 		setcookie ($sessionid, "", time()-3600);
+		setcookie ("bm_login_cookie", time()-3600);
 		return true;
 	}
 
@@ -60,11 +78,12 @@ class users {
 		$md_pass = md5($pass);
 		$now = time();
 		$this->db->execute("insert into users(username,email,password,join_date) values('$user','$email','$md_pass', $now)");
+		$this->db->execute("insert into blogmemes_portugues.users(username,email,password,join_date) values('$user','$email','$md_pass', $now)");
 		return $this->do_login($user, $pass);
 	}
 
 
-	function do_login($user_name, $pass)
+	function do_login($user_name, $pass, $remember=false)
 	{
 		$user_name = sanitize(strtolower($user_name));
 
@@ -72,6 +91,13 @@ class users {
 		if ($user->password == md5($pass)) 
 		{
 			$_SESSION['user_data'] = $user;
+			if ($remember)
+			{
+				$psession = md5($user->user_name . '-' . time());
+				$uid = $user->ID;
+				$this->db->execute("update users set persistent_session = '$psession' where ID = $uid");
+				setcookie("bm_login_cookie", $psession, time()+86400*30, '/', false);
+			}
 			$this->user = $user;
 			return true;
 		}          
@@ -87,6 +113,14 @@ class users {
 		$sql .= ' blog = '.sanitize($data['blog']).' ';
 		$sql .= ' where ID = '.sanitize($data['user_id']);
 		$this->db->execute($sql);
+		$sql  = ' update blogmemes_portugues.users set ';
+		$sql .= ' email = '.sanitize($data['email']).',';
+		$sql .= ' fullname= '.sanitize($data['fullname']).',';
+		$sql .= ' website = '.sanitize($data['website']).',';
+		$sql .= ' blog = '.sanitize($data['blog']).' ';
+		$sql .= ' where ID = '.sanitize($data['user_id']);
+		$this->db->execute($sql);
+
 
 		$this->user->email = $data['email'];
 		$this->user->fullname = $data['fullname'];
@@ -137,6 +171,8 @@ class users {
 		}
 		$sql = 'update users set password = \''.md5($new_pass).'\' where ID = '.$user_id;
  	$this->db->execute($sql);
+	$sql = 'update blogmemes_portugues.users set password = \''.md5($new_pass).'\' where ID = '.$user_id;
+$this->db->execute($sql);
 
 		return true;
 	}
@@ -150,6 +186,8 @@ class users {
 			$pass = substr(base64_encode(md5($user->password.$user->email.$user->id.time())), 0, 8);
 			$sql = 'update users set password = \''.md5($pass).'\'  where ID = '.$user->ID;
 			$this->db->execute($sql);
+			$sql = 'update blogmemes_portugues.users set password = \''.md5($new_pass).'\' where ID = '.$user->ID;
+		$this->db->execute($sql);
 			mail($email, $subject, sprintf($body, $user->username, $pass, $login_url));
 		}
 		return $pass;
@@ -165,6 +203,39 @@ class users {
 	{
 		$sql = 'select count(*) from users where username = \''.$username.'\'';
 		return $this->db->fetch_scalar($sql);
+	}
+
+	function get_random_profile_links($n)
+	{
+		$result = array();
+		$users = $this->db->fetch("select username, email from users order by rand() limit $n");
+		foreach ($users as $user)
+		{
+			$user->gravatar = get_gravatar($bm_url, $user->email, 40);
+			$result[] = '<a href="profile.php?user_name='.$user->username.'"><img border="0" src="'.$user->gravatar.'" alt="'.$user->username.'" /></a>'
+				.'<br/><a style="font-size:10px" href="profile.php?user_name='.$user->username.'">'.
+				substr($user->username,0,10).'</a>';
+		}
+		return $result;
+	}
+
+	function get_profile_links($n)
+	{
+		$result = array();
+		if ($n > 0) {
+			$users = $this->db->fetch("select username, email from users u order by rand() limit $n");
+		}
+		else {
+			$users = $this->db->fetch("select u.username, u.email, count(p.ID) count_posts from users u left join posts p on p.submitted_user_id = u.ID group by u.username, u.email order by count_posts desc");
+		}
+		foreach ($users as $user)
+		{
+			$user->gravatar = get_gravatar($bm_url, $user->email, 40);
+			$result[] = '<a href="profile.php?user_name='.$user->username.'"><img border="0" src="'.$user->gravatar.'" alt="'.$user->username.'" /></a>'
+				.'<br/><a style="font-size:10px" href="profile.php?user_name='.$user->username.'">'.
+				substr($user->username,0,10).'</a>';
+		}
+		return $result;
 	}
 
 }
