4bafc79a3acc7b950169e27a55980f013963bea7
eadiaz <eadiaz@8bafb967-bd0d-0410-aeb5-8d584dead05f>
2006-03-27 16:16:47 +0000

    git-svn-id: svn://svn.code.sf.net/p/akarru/code/trunk@64 8bafb967-bd0d-0410-aeb5-8d584dead05f

--- public_html/config.php
+++ public_html/config.php
@@ -10,7 +10,7 @@ define('DATABASE_NAME', "");
 $bm_promo_level = 7;
 $bm_main_feeds = '';
 $bm_queue_feeds = '';
-$bm_url_feeds = ''';
+$bm_url_feeds = '';
 $bm_lang = 'es-es';
 $bm_url  = 'http://www.blogmemes.com/';
 $bm_desc = 'Blog Memes es un servicio de promocion colectiva de urls';
