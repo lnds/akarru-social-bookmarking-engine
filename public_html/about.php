5559ebf71fcd5d0707292d0b4664b44a34a7f21d
eadiaz <eadiaz@8bafb967-bd0d-0410-aeb5-8d584dead05f>
2006-03-27 16:12:07 +0000

    eliminates hard coded text on about.php
    git-svn-id: svn://svn.code.sf.net/p/akarru/code/trunk@60 8bafb967-bd0d-0410-aeb5-8d584dead05f

--- public_html/about.php
+++ public_html/about.php
@@ -1,6 +1,6 @@
 <?php
   include_once('akarru.lib/common.php');
-  $smarty->assign('content_title', 'quienes somos');
+  $smarty->assign('content_title', $bl_about);
   $smarty->assign('content', 'about');
   $smarty->display('master_page.tpl');
 ?>
