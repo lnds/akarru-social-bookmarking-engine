cc80530d952785e1fc6d0fe254fa60609aae8326
eadiaz <eadiaz@8bafb967-bd0d-0410-aeb5-8d584dead05f>
2006-03-27 16:15:29 +0000

    add debate room.
    git-svn-id: svn://svn.code.sf.net/p/akarru/code/trunk@63 8bafb967-bd0d-0410-aeb5-8d584dead05f

--- public_html/comment.php
+++ public_html/comment.php
@@ -13,14 +13,25 @@
   $memes = new memes($bm_db, $bm_user);
   if (!empty($_POST)) {
 	  $memes->add_comment($meme_id, $_POST['comment']);
+	  if (isset($_POST['position'])) {
+		  $memes->debate($meme_id, $bm_user, $_POST['position'], false);
+
+	  }
+	  header("Location: comment.php?meme_id=$meme_id");
+	  exit();
+	  return;
   }
+  $memes->debate($meme_id, $bm_user, 0, true);
   $meme = $memes->get_meme($meme_id);
   $comments = $memes->get_comments($meme_id);
   $smarty->assign('meme', $meme);
   $smarty->assign('meme_id', $meme_id);
+  $smarty->assign('community', true);
   $smarty->assign('content', 'comment');
   $smarty->assign('comments', $comments);         
   $smarty->assign('page', $page);
-  $smarty->assign('voters', $memes->get_voters($meme_id));
+  $smarty->assign('friends', $memes->get_friends($meme_id));
+  $smarty->assign('foes', $memes->get_foes($meme_id));
+  $smarty->assign('neutrals', $memes->get_neutrals($meme_id));
   $smarty->display('master_page.tpl');
 ?>
