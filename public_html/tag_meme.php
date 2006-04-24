67bbce05f3275ac423c70845339b4f6271ca9d5c
eadiaz <eadiaz@8bafb967-bd0d-0410-aeb5-8d584dead05f>
2006-04-24 16:59:54 +0000

    BUG FIX: tags where lost.
    git-svn-id: svn://svn.code.sf.net/p/akarru/code/trunk@115 8bafb967-bd0d-0410-aeb5-8d584dead05f

--- public_html/tag_meme.php
+++ public_html/tag_meme.php
@@ -14,7 +14,13 @@
   $meme = $memes->get_meme($meme_id);
   $smarty->assign('meme', $meme);
   $smarty->assign('meme_id', $meme_id);
-  $smarty->assign('meme_tags', $memes->get_tags($meme_id));
+  $tags = $memes->get_tags($meme_id);
+  $memes_tags = array();
+  foreach ($tags as $tag)
+  {
+	  $memes_tags[] = '&nbsp;<a href="memes_by_tag.php?tag_id='.$tag->ID.'">'.$tag->tag.'</a>&nbsp;&nbsp;';
+  }
+  $smarty->assign('meme_tags', $memes_tags);
   $smarty->assign('content', 'tag_meme');
   $smarty->display('master_page.tpl');
 ?>
