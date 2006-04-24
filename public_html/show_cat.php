250280a3bf39a3af39143ad11c3226a6a1846336
eadiaz <eadiaz@8bafb967-bd0d-0410-aeb5-8d584dead05f>
2006-04-24 16:58:24 +0000

    add default feeds for categories when a feed url is not present on database.
    git-svn-id: svn://svn.code.sf.net/p/akarru/code/trunk@114 8bafb967-bd0d-0410-aeb5-8d584dead05f

--- public_html/show_cat.php
+++ public_html/show_cat.php
@@ -5,12 +5,16 @@
   $page = $_GET['page'];
   $cat = $memes->get_category($cat_id);
   $smarty->assign('content_title', $cat->cat_title);
-  $smarty->assign('content_feed', 'cat_feed.php?cat_id='.$cat_id);
+  $feed_url = "cat_feed.php?cat_id=$cat_id";
+  $smarty->assign('content_feed', $feed_url);
   $smarty->assign('query_ext', '&cat_id='.$cat_id);
   $smarty->assign('memes', $memes->get_memes_by_category($cat_id, $page));
   if ($memes->pages > 1) 
 	  $smarty->assign('pages', $memes->pages+1);
-  $smarty->assign('content_feed_link', $cat->feed);
+  if (empty($cat->feed)) 
+	  $smarty->assign('content_feed_link', "<a href=\"$feed_url\">".'<img src="http://www.feedburner.com/fb/images/pub/feed-icon16x16.png" border="0"/></a>');
+  else
+	  $smarty->assign('content_feed_link', $cat->feed);
   $smarty->assign('content', 'memes_grid');
   $smarty->display('master_page.tpl');
 ?>
