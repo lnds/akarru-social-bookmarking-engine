f066c9e472b13f8ef2444f22370e98482e4726f1
eadiaz <eadiaz@8bafb967-bd0d-0410-aeb5-8d584dead05f>
2006-04-24 16:54:33 +0000

    BUG FIX: paging wasn't working properly
    git-svn-id: svn://svn.code.sf.net/p/akarru/code/trunk@110 8bafb967-bd0d-0410-aeb5-8d584dead05f

--- public_html/index.php
+++ public_html/index.php
@@ -1,15 +1,17 @@
 <?
   include_once('akarru.lib/common.php');
   $page = $_GET['page'];
-  $smarty->assign('content_title', $bl_last_memes);
   $memes = new memes($bm_db, $bm_user, $bm_promo_level);
+  $data = $memes->get_memes($page, '');
+  
 
-  $smarty->assign('memes', $memes->get_memes($page, ''));
-  if ($memes->pages > 50) {
+  $smarty->assign('content_title', $bl_last_memes);
+  $smarty->assign('memes', $data);
+  if ($memes->pages > 50) 
 	  $memes->pages = 50;
-  }
   if ($memes->pages > 1) 
 	  $smarty->assign('pages', $memes->pages+1);
+  $smarty->assign('page', $page);
   $smarty->assign('$bm_message', $bl_promoted_message);
   $smarty->assign('content', 'memes_grid');
   $smarty->assign('content_feed_link', $bm_main_feeds);
