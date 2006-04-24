51a71ed0035b4927719cac8f56fe6aa3b5ea52a8
eadiaz <eadiaz@8bafb967-bd0d-0410-aeb5-8d584dead05f>
2006-04-24 16:49:12 +0000

    Gravatar md5 hash is stored directly in database now. More easy to read implementations of fucntions. Influence and popularity are disabled in this release, they are causing an slow display of the profile page. Fixed bug on AES encryption, when passwords are stored, a global key is required now.
    git-svn-id: svn://svn.code.sf.net/p/akarru/code/trunk@107 8bafb967-bd0d-0410-aeb5-8d584dead05f

--- public_html/akarru.lib/users.php
+++ public_html/akarru.lib/users.php
@@ -23,13 +23,9 @@ class users {
 		$psession = $_COOKIE["bm_login_cookie"];
 		$pers_user = $this->db->fetch_object("select * from users where persistent_session = '$psession'");
 		if ($pers_user->ID > 0)
-		{
 			$this->user = $pers_user;
-		}
 		else
-		{
 			$this->user = $_SESSION['user_data'];
-		}
 		return isset($this->user);
 	}
 
@@ -49,40 +45,34 @@ class users {
 
 	function get_user_id()
 	{
-		if (isset($this->user)) 
-			return $this->user->ID;
-		return '';
+		return $this->user->ID;
 	}
 
 	function get_is_admin()
 	{
-		if (isset($this->users)) {
-			return $this->users->admin == 1;
-		}
-		return false;
+		return $this->users->admin == 1;
 	}
 
 
 	function get_user_name()
 	{
-		if (isset($this->user)) 
-			return $this->user->username;
-		return '';
+		return $this->user->username;
 	}
 
 	function register_user($user, $email, $pass)
 	{
-		if ($this->check_user_exists($user)) {
-			return false;
-		}
-		if ($this->check_email_exists($email)) {
-			return false;
-		}
+		if ($this->check_user_exists($user)) 
+			return FALSE;
+		
+		if ($this->check_email_exists($email)) 
+			return FALSE;
+		
 		$user = htmlspecialchars($user);
 		$email = htmlspecialchars($email);
 		$now = time();
 		$key = $this->aes_key;
-		$this->db->execute("insert into users(username,email,strong_pass,join_date) values('$user','$email',aes_encrypt(md5('$pass'), md5($now)), $now)");
+		$this->db->execute("insert into users(username,email,strong_pass,join_date,gravatar) values('$user','$email', aes_encrypt(md5('$pass'), md5($now || '$key')), $now, md5('$email'))");
+
 		return $this->do_login($user, $pass, false);
 	}
 
@@ -90,32 +80,30 @@ class users {
 	function do_login($user_name, $pass, $remember)
 	{
 		$user_name = sanitize(strtolower($user_name));
-
 		$user = $this->db->fetch_object("select ID, join_date from users where lower(username)=$user_name");
 		if (empty($user->ID)) 
 			return false;
-
-		$user = $this->db->fetch_object("select * from users where lower(username)=$user_name and aes_decrypt(strong_pass, md5($user->join_date)) = md5('$pass')");
-		if (!empty($user->ID))
+		$key = $this->aes_key;
+		$user = $this->db->fetch_object("select * from users where lower(username)=$user_name and aes_decrypt(strong_pass, md5(join_date || '$key')) = md5('$pass')");
+		if (empty($user->ID))
+			return false;
+		$_SESSION['user_data'] = $user;
+		if (!empty($remember))
 		{
-			$_SESSION['user_data'] = $user;
-			if (!empty($remember))
-			{
-				$psession = md5($user->user_name . '-' . time());
-				$uid = $user->ID;
-				$this->db->execute("update users set persistent_session = '$psession' where ID = $uid");
-				setcookie("bm_login_cookie", $psession, time()+86400*30, '/', false);
-			}
-			$this->user = $user;
-			return true;
-		}          
-		return false;
+			$psession = md5($user->user_name . '-' . time());
+			$uid = $user->ID;
+			$this->db->execute("update users set persistent_session = '$psession' where ID = $uid");
+			setcookie("bm_login_cookie", $psession, time()+86400*30, '/', false);
+		}
+		$this->user = $user;
+		return true;
 	}
 
 	function update_profile($data)
 	{
 		$sql  = ' update users set ';
 		$sql .= ' email = '.sanitize($data['email']).',';
+		$sql .= ' gravatar = md5('.sanitize($data['email']).'), ';
 		$sql .= ' fullname= '.sanitize($data['fullname']).',';
 		$sql .= ' website = '.sanitize($data['website']).',';
 		$sql .= ' blog = '.sanitize($data['blog']).' ';
@@ -133,20 +121,19 @@ class users {
 
 	function get_user_profile($user_name, $memes, $promote_level = 5)
 	{
-		$sql  = " select id from users where username = '$user_name' ";
-		$id = $this->db->fetch_scalar($sql);
+		$id = $this->db->fetch_scalar(" select id from users where username = '$user_name' ");
 		return $this->get_user_profile_by_id($id, $memes, $promote_level);
 	}
 
 
 	function get_user_profile_by_id($user_id, $memes, $promote_level = 5)
 	{
-		$sql  = ' select u.id, u.username, u.email, u.fullname, u.blog, u.join_date, u.website ';
+		$sql  = ' select u.id, u.username, u.gravatar, u.fullname, u.blog, u.join_date, u.website ';
 		$sql .= ' from users u left join posts p on p.submitted_user_id = u.id ';
 		$sql .= " where u.id = $user_id ";
 		$profile = $this->db->fetch_object($sql);
 		$stats = new statistics($this->db);
-		$infl  = $stats->calc_influence($profile->id);
+	   // $infl  = $stats->calc_influence($profile->id);
 		$profile->memes = $infl->memes;
 		$profile->votes = $infl->votes;
 		$profile->comments = $infl->comments;
@@ -158,8 +145,8 @@ class users {
 		$profile->promoted_memes = $memes->memes_count;
 
 		// gravatar
-		$profile->gravatar = get_gravatar($bm_url, $profile->email, 80); 
-		$profile->small_gravatar = get_gravatar($bm_url, $profile->email, 40);  
+		$profile->small_gravatar = get_gravatar($bm_url, $profile->gravatar, 40);
+		$profile->gravatar = get_gravatar($bm_url, $profile->gravatar, 80);
 
 
 		return $profile;
@@ -167,18 +154,16 @@ class users {
 
 	function change_password($user_id, $new_pass, $confirm_pass)
 	{
-		if ($new_pass != $confirm_pass) {
-			return false;
-		}
-		$sql = "update users set strong_pass = aes_encrypt(md5('$new_pass'), md5(join_date || '$key')) where ID = $user_id";
-		$this->db->execute($sql);
+		if ($new_pass != $confirm_pass) 
+			return FALSE;
+		$key = $this->aes_key;
+		$this->db->execute("update users set strong_pass = aes_encrypt(md5('$new_pass'), md5(join_date || '$key')) where ID = $user_id");
 		return true;
 	}
 
 	function gen_password($email, $subject, $body, $login_url)
 	{
-		$sql = " select * from users where email = '$email' limit 1";
-		$user = $this->db->fetch_object($sql);
+		$user = $this->db->fetch_object("select * from users where email = '$email' limit 1");
 		if ($user) 
 		{
 			$pass = substr(base64_encode(md5($user->password.$user->email.$user->id.time())), 0, 8);
@@ -203,10 +188,10 @@ class users {
 	function get_random_profile_links($n)
 	{
 		$result = array();
-		$users = $this->db->fetch("select username, email from users order by rand() limit $n");
+		$users = $this->db->fetch("select username, gravatar from users order by rand() limit $n");
 		foreach ($users as $user)
 		{
-			$user->gravatar = get_gravatar($bm_url, $user->email, 40);
+			$user->gravatar = get_gravatar($bm_url, $user->gravatar, 40);
 			$result[] = '<a href="profile.php?user_name='.$user->username.'"><img border="0" src="'.$user->gravatar.'" alt="'.$user->username.'" /></a>'
 				.'<br/><a style="font-size:10px" href="profile.php?user_name='.$user->username.'">'.
 				substr($user->username,0,10).'</a>';
@@ -217,15 +202,13 @@ class users {
 	function get_profile_links($n)
 	{
 		$result = array();
-		if ($n > 0) {
-			$users = $this->db->fetch("select username, email from users u order by rand() limit $n");
-		}
-		else {
-			$users = $this->db->fetch("select u.username, u.email from users u order by u.ID asc");
-		}
+		if ($n > 0) 
+			$users = $this->db->fetch("select username, gravatar from users u order by rand() limit $n");
+		else 
+			$users = $this->db->fetch("select u.username, u.gravatar from users u order by u.ID asc");
 		foreach ($users as $user)
 		{
-			$user->gravatar = get_gravatar($bm_url, $user->email, 40);
+			$user->gravatar = get_gravatar($bm_url, $user->gravatar, 40);
 			$result[] = '<a href="profile.php?user_name='.$user->username.'"><img border="0" src="'.$user->gravatar.'" alt="'.$user->username.'" /></a>'
 				.'<br/><a style="font-size:10px" href="profile.php?user_name='.$user->username.'">'.
 				substr($user->username,0,10).'</a>';
