275295e2ed0dae8ea22b6ad41dc319feea3edb45
eadiaz <eadiaz@8bafb967-bd0d-0410-aeb5-8d584dead05f>
2006-06-16 19:30:58 +0000

    added social tracking of memes.
    git-svn-id: svn://svn.code.sf.net/p/akarru/code/trunk@173 8bafb967-bd0d-0410-aeb5-8d584dead05f

--- public_html/redirect.php
+++ public_html/redirect.php
@@ -5,7 +5,13 @@ $meme_id = intval($_GET['meme_id']);
 if($meme_id > 0)
 {
 	$bm_memes = new memes($bm_db, $bm_user, $bm_promo_level);
-	$bm_memes->click($meme_id, $bm_user);
+	$url = $bm_memes->click($meme_id, $bm_user);
 }
-
+else
+{
+	$url = "/index.php";
+}
+header("Location: $url");
+exit();
+return;
 ?>
--- public_html/social_click.php
+++ public_html/social_click.php
@@ -1,13 +1,11 @@
 <?php
 
 include_once('akarru.lib/common.php');
-
-if(isset($_GET['meme_id']))
+$meme_id = intval($_GET['meme_id']);
+if($meme_id > 0)
 {
-    $meme_id = (int) $_GET['meme_id'];
 	$bm_memes = new memes($bm_db, $bm_user, $bm_promo_level);
 	$bm_memes->social_click($meme_id, $bm_user);
 }
 
 ?>
-
