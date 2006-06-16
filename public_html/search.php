be6bf726b68645195e9154b105a1fbd9d3a07229
eadiaz <eadiaz@8bafb967-bd0d-0410-aeb5-8d584dead05f>
2006-06-16 20:18:57 +0000

    more secure version of this script.
    git-svn-id: svn://svn.code.sf.net/p/akarru/code/trunk@184 8bafb967-bd0d-0410-aeb5-8d584dead05f

--- public_html/search.php
+++ public_html/search.php
@@ -1,17 +1,19 @@
 <?
   include_once('akarru.lib/common.php');
-  $page = $_GET['page'];
   $smarty->assign('content_title', $bl_search);
+  $smarty->assign('page_title', 'blogmemes - '.$bl_search);
+
   $memes = new memes($bm_db, $bm_user, $bm_promo_level);
   if (empty($_POST)) {
 	  $search = $_GET['search'];
-	  $smarty->assign('memes', $memes->get_memes($page));
+	  $smarty->assign('memes', $memes->get_memes($bm_page));
   }
   else
   {
 	  $search = $_POST['search'];
 	  $smarty->assign('memes', $memes->search_memes($search));
   }
+  $search = sanitize($search);
   if ($memes->pages > 1) 
 	  $smarty->assign('pages', $memes->pages+1);
   $smarty->assign('bm_message', 'criterio de b&uacute;squeda: '.$search);
