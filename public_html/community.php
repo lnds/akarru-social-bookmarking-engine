f602648de83505539c482bd06a37353ee1010990
eadiaz <eadiaz@8bafb967-bd0d-0410-aeb5-8d584dead05f>
2006-06-16 19:35:10 +0000

    added pagination on community list.
    git-svn-id: svn://svn.code.sf.net/p/akarru/code/trunk@175 8bafb967-bd0d-0410-aeb5-8d584dead05f

--- public_html/community.php
+++ public_html/community.php
@@ -1,7 +1,11 @@
 <?php
   include_once('akarru.lib/common.php');
+  $smarty->assign('page_title', 'blogmemes - comunidad');
   $smarty->assign('content_title', $bl_community);
-  $smarty->assign('community', $bm_users->get_profile_links(0));
+  $page_size = 120;
+  $pages = ceil($bm_users->count_users()/$page_size);
+  $smarty->assign('pages', $pages+1);
+  $smarty->assign('community', $bm_users->get_profile_links(0, $bm_page,$page_size));
   $smarty->assign('content', 'community_grid');
   $smarty->assign('content_feed_link', $bm_main_feeds);
   $smarty->assign('show_ads', true);
