0994411e37df34aab0bcf716e0890adede54f87f
eadiaz <eadiaz@8bafb967-bd0d-0410-aeb5-8d584dead05f>
2006-06-16 19:14:12 +0000

    Change sender of all emails.
    git-svn-id: svn://svn.code.sf.net/p/akarru/code/trunk@166 8bafb967-bd0d-0410-aeb5-8d584dead05f

--- public_html/akarru.lib/mail.php
+++ public_html/akarru.lib/mail.php
@@ -2,6 +2,7 @@
 
 function send_new_password($email, $pass, $site, $login_url)
 {
+    ini_set(sendmail_from,'admin@blogmemes.com'); 
 	@mail($email, '['.$bm_site_name.'] '. $bl_recover_pass_subject, sprintf($bf_recover_pass, $pass));
 }
 ?>
