4a5670dffa3e7fea8983f6fc78ff4eec0bcc3b7a
eadiaz <eadiaz@8bafb967-bd0d-0410-aeb5-8d584dead05f>
2006-06-16 20:13:37 +0000

    Fix security flaw and uses the new $bm_page variable.
    git-svn-id: svn://svn.code.sf.net/p/akarru/code/trunk@181 8bafb967-bd0d-0410-aeb5-8d584dead05f

--- public_html/index.php
+++ public_html/index.php
@@ -1,17 +1,17 @@
 <?
   include_once('akarru.lib/common.php');
-  $page = $_GET['page'];
   $memes = new memes($bm_db, $bm_user, $bm_promo_level);
-  $data = $memes->get_memes($page, '');
+  $data = $memes->get_memes($bm_page, '');
   
 
   $smarty->assign('content_title', $bl_last_memes);
   $smarty->assign('memes', $data);
+
+  $smarty->assign('page_title', 'blogmemes - '.$data[0]->title);
   if ($memes->pages > 50) 
 	  $memes->pages = 50;
   if ($memes->pages > 1) 
 	  $smarty->assign('pages', $memes->pages+1);
-  $smarty->assign('page', $page);
   $smarty->assign('$bm_message', $bl_promoted_message);
   $smarty->assign('content', 'memes_grid');
   $smarty->assign('content_feed_link', $bm_main_feeds);
--- public_html/memes_queue.php
+++ public_html/memes_queue.php
@@ -2,16 +2,15 @@
   include_once('akarru.lib/common.php');
   $smarty->assign('content_title', 'votar pendientes');
   $memes = new memes($bm_db, $bm_user, $bm_promo_level);
-  $smarty->assign('memes', $memes->get_new_memes($page));
+  $data = $memes->get_new_memes($bm_page);
+  $smarty->assign('page_title', 'blogmemes - '.$data[0]->title);
+  $smarty->assign('memes', $data);
   $smarty->assign('bm_message', '<em>Vota por estos memes para que aparezcan en la primera página</em>');
   if ($memes->pages > 1) 
 	  $smarty->assign('pages', $memes->pages+1);
   $smarty->assign('content_feed_link', $bm_queue_feeds);
   $smarty->assign('show_ads', true);
   $smarty->assign('content', 'memes_grid');
-  $smarty->assign('show_ads', true);
-  $smart_id = $_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING'];
-  $smarty->assign('pag_id', 'new');
-  $smarty->display('master_page.tpl', $smart_id);
+  $smarty->display('master_page.tpl');
 ?>
 
