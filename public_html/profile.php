2033af349625b7ae4eac08e5bc4d402759900050
eadiaz <eadiaz@8bafb967-bd0d-0410-aeb5-8d584dead05f>
2006-06-16 19:48:42 +0000

    Profile now contains search of votes and comentaries sent by user.
    git-svn-id: svn://svn.code.sf.net/p/akarru/code/trunk@179 8bafb967-bd0d-0410-aeb5-8d584dead05f

--- public_html/profile.php
+++ public_html/profile.php
@@ -6,14 +6,28 @@
 	  exit();
 	  return;
   }
-  $page = $_GET['page'];
-  if (empty($page)) {
-	  $page = 0;
-  }
-  $memes = new memes($bm_db, $bm_user, 0, 5);
+  $memes = new memes($bm_db, $bm_user, 0, 15);
   $profile = $bm_users->get_user_profile($user_name, $memes);
   $smarty->assign('user_profile', $profile);
-  $smarty->assign('memes', $memes->get_memes_by_user($profile->id, $page));
+  $view = $_GET['view'];
+  if ($view == "c") {
+	  $data = $memes->get_commented_memes_by_user($bm_page, $profile->id);
+	  $bl_profile = 'estos son los memes comentados por ';
+	  $smarty->assign('query_ext', '&view=c&user_name='.$user_name);
+  }
+  else if ($view == 'v') {
+	  $data = $memes->get_voted_memes_by_user($bm_page, $profile->id);
+	  $bl_profile = 'estos son los memes votados por ';
+	  $smarty->assign('query_ext', '&view=v&user_name='.$user_name);
+  }
+  else {
+	  $data = $memes->get_memes_by_user($profile->id, $bm_page);
+	  $bl_profile = 'estos son los memes enviados por ';
+	  $smarty->assign('query_ext', '&user_name='.$user_name);
+
+  }
+  $smarty->assign('memes', $data);
+  $smarty->assign('page_title', 'blogmemes - '.$user_name);
 
 
   if ($memes->pages > 1) 
@@ -21,6 +35,5 @@
   $smarty->assign('content_title', $bl_profile.' '.$profile->username);
   $smarty->assign('content', 'profile');
   $smarty->assign('page', $page);
-  $smarty->assign('query_ext', '&user_name='.$user_name);
   $smarty->display('master_page.tpl');
 ?>
