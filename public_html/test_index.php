50d8240d3e84c3ba17259724de91b3a6e8b8fc41
eadiaz <eadiaz@8bafb967-bd0d-0410-aeb5-8d584dead05f>
2006-06-16 19:32:31 +0000

    this script is used for benchmarking, and performance tunning
    git-svn-id: svn://svn.code.sf.net/p/akarru/code/trunk@174 8bafb967-bd0d-0410-aeb5-8d584dead05f

---
+++ public_html/test_index.php
@@ -0,0 +1,35 @@
+<?
+function microtime_float()
+{
+   list($usec, $sec) = explode(" ", microtime());
+   return ((float)$usec + (float)$sec);
+}
+
+$time_start = microtime_float();
+  include_once('akarru.lib/common.php');
+$time_load_common = microtime_float();
+
+  $page = $_GET['page'];
+  $memes = new memes($bm_db, $bm_user, $bm_promo_level);
+  $data = $memes->get_memes($page, '');
+  
+$time_data_processing = microtime_float();
+  $smarty->assign('content_title', $bl_last_memes);
+  $smarty->assign('memes', $data);
+  if ($memes->pages > 50) 
+	  $memes->pages = 50;
+  if ($memes->pages > 1) 
+	  $smarty->assign('pages', $memes->pages+1);
+  $smarty->assign('page', $page);
+  $smarty->assign('$bm_message', $bl_promoted_message);
+  $smarty->assign('content', 'memes_grid');
+  $smarty->assign('content_feed_link', $bm_main_feeds);
+  $smarty->assign('show_ads', true);
+  $smarty->display('master_page.tpl');
+$time_smarty = microtime_float();
+
+echo $time_load_common - $time_start.',';
+echo $time_data_processing - $time_load_common.',';
+echo $time_smarty - $time_data_processing;
+?>
+
