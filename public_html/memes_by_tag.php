61e4a6c2b72137d620a34f3a3272bd4893635d5f
eadiaz <eadiaz@8bafb967-bd0d-0410-aeb5-8d584dead05f>
2006-06-16 19:47:58 +0000

    pagination support
    git-svn-id: svn://svn.code.sf.net/p/akarru/code/trunk@178 8bafb967-bd0d-0410-aeb5-8d584dead05f

--- public_html/memes_by_tag.php
+++ public_html/memes_by_tag.php
@@ -1,12 +1,22 @@
 <?php
   include_once('akarru.lib/common.php');
-  $tag_id = $_GET['tag_id'];
-  $page   = $_GET['page'];
+  $tag_id = intval($_GET['tag_id']);
   $bm_no_folkbar = true;
-  $memes = new memes($bm_db, $bm_user, 0);
-  $bm_title = 'Etiqueta: '.$memes->get_tag_name($tag_id);
+  $memes = new memes($bm_db, $bm_user);
+  if ($tag_id > 0) {
+	  $bm_title = 'Etiqueta: '.$memes->get_tag_name($tag_id);
+  }
+  else
+  {
+	  $tag_name = $_GET['tag_name'];
+	  $bm_title = 'Etiqueta: '.$tag_name;
+	  $tag_id = $memes->get_tag_id($tag_name);
+  }
   $smarty->assign('content_title', $bm_title);
-  $smarty->assign('memes', $memes->get_memes_by_tag($tag_id,  $page, 'order by date_promo desc, votes desc'));
+  $data = $memes->get_memes_by_tag($tag_id,  $bm_page, 'order by date_promo desc, votes desc');
+  $smarty->assign('page_title', 'blogmemes - '.$data[0]->title);
+  $smarty->assign('memes', $data);
+  $smarty->assign('pages',$memes->pages+1);
   $smarty->assign('content', 'memes_grid');
   $smarty->display('master_page.tpl');
 ?>
