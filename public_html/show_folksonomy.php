a34ffca6a4317da19096d3f8a891b665271bcd60
eadiaz <eadiaz@8bafb967-bd0d-0410-aeb5-8d584dead05f>
2006-06-16 20:20:07 +0000

    add pagination to folksonomy.
    git-svn-id: svn://svn.code.sf.net/p/akarru/code/trunk@185 8bafb967-bd0d-0410-aeb5-8d584dead05f

--- public_html/show_folksonomy.php
+++ public_html/show_folksonomy.php
@@ -2,7 +2,8 @@
   include_once('akarru.lib/common.php');
   $smarty->assign('content_title', 'folksonom&iacute;a');
   $smarty->assign('content', 'folksonomy_grid');
-  $smarty->assign('all_tags', $bm_tags->fetch_all());
+  $smarty->assign('all_tags', $bm_tags->fetch_all($bm_page));
+  $smarty->assign('pages', $bm_tags->count_pages()+1);
   $smarty->display('master_page.tpl');
 ?>
 
