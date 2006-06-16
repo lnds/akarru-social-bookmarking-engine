dc41f1596a7dbd5a1e01f1b47c02f4548031551d
eadiaz <eadiaz@8bafb967-bd0d-0410-aeb5-8d584dead05f>
2006-06-16 19:23:32 +0000

    Now we officially support utf-8 only.
    git-svn-id: svn://svn.code.sf.net/p/akarru/code/trunk@170 8bafb967-bd0d-0410-aeb5-8d584dead05f

--- public_html/cat_feed.php
+++ public_html/cat_feed.php
@@ -10,7 +10,7 @@ header("Content-Type: text/xml");
 
 $bm_memes = new memes($bm_db, $bm_user);
 $memes = $bm_memes->get_memes_by_category($cat_id, $page, '', $page == 0? 200 : 0);
-print '<?xml version="1.0" encoding="ISO-8859-1" standalone="yes" ?>';
+print '<?xml version="1.0" encoding="utf-8" standalone="yes" ?>';
 ?>
 <rss version="2.0">
 <channel>
