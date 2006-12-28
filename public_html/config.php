ad002fe82b4f23d82bd3af603f4ae2975a218a0c
eadiaz <eadiaz@8bafb967-bd0d-0410-aeb5-8d584dead05f>
2006-12-28 20:06:23 +0000

    few modifications to config.php.
    git-svn-id: svn://svn.code.sf.net/p/akarru/code/trunk@266 8bafb967-bd0d-0410-aeb5-8d584dead05f

--- public_html/config.php
+++ public_html/config.php
@@ -1,38 +1,39 @@
 <?php
 
-	
 define('DATABASE_USER', '');
 define('DATABASE_PASS', "");
-define('DATABASE_HOST', "");
+define('DATABASE_HOST', "localhost");
 define('DATABASE_NAME', "");
 define('PROMO_LEVEL', '5');
-define('RECORDS_TO_PAGE', '15');
+define('RECORDS_TO_PAGE', '10');
 define('DOMAIN', 'blogmemes.com'); // domain for cookies
 define('AES_KEY', '');   /// AES site key for password storage
+define('VIDEO', '2');
 
+define('RSS_FEEDS', '');
+define('SITE_CAPTION', 'BlogMemes');
 
 $bm_promo_level = 5;
+
+
+
 $bm_main_feeds = '';
 $bm_queue_feeds = '';
 $bm_url_feeds = '';
-$bm_url_queue_feeds = '';
 $bm_lang = 'es-es';
 $bm_url  = 'http://www.blogmemes.com/';
-$bm_domain = 'blogmemes.com';
-$bm_web_address = 'www.blogmemes.com';
-$bm_admin_email_address = 'admin@blogmemes.com';
 $bm_desc = 'Blog Memes es un servicio de promocion colectiva de urls';
 $bm_site_name = 'Blog Memes';
 $bm_home = 'index.php';
+$bm_ajax_service = 'http://www.blogmemes.com/ajax_vote.php';
+$bm_login_url =  'http://www.blogmemes.com/login.php'; 
 
-$bm_date_posted_format='Published on %d/%m/%Y at %R';
+$bf_recover_pass = "Le informamos que usted o alguien a su nombre a solicitado la recuperacion de clave.
+Su identificacion de usuario es: %s
+Su nueva clave es: %s
+Visite esta direcci&oacute;: %s para ingresar con su nueva clave
+Usted puede cambiar su clave modificando su perfil
+";
 
-$bm_ajax_service = '';
-$bm_login_url =  '';
-
-// You need to get your own API key for Akismet. The easiest way is to register a wordpress blog at wordpress.com
-//  Then follow the instructions at http://faq.wordpress.com/2005/10/19/api-key/
-// 
 $bm_akismet_api_key = '';
-
 ?>
